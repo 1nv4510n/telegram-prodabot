ad083b3ec5e46035bedf0fa0e9f445ea9d3f9f68
1nv4510n <bogdan226470@gmail.com>
2022-10-03 19:38:54 +0400

    error handler fix

--- bot/handlers/errors/error_handler.py
+++ bot/handlers/errors/error_handler.py
@@ -3,10 +3,11 @@ from aiogram.types import TelegramObject
 from bot.utils.logging import log
 
 from aiogram.exceptions import TelegramForbiddenError
+from aiogram.types.error_event import ErrorEvent
 
 router = Router()
 
 @router.errors()
-async def forbidden_error_handler(update: TelegramObject, error: Exception) -> None:
-    if isinstance(error, TelegramForbiddenError):
-        log.warning(f'[{error}] Send default message error. Bot blocked by user.')
+async def forbidden_error_handler(error: ErrorEvent) -> None:
+    if isinstance(error.exception, TelegramForbiddenError):
+        log.warning(f'[{error.exception}] Send default message error. Bot blocked by user.')
